370a6ccf34c9c416467fe0123302ebb882cac4d4
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-02-10 21:30:09 +0100

    Filter
    eliminar thinks not use in Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
 import { setStatusFilter } from 'redux/filterSlice';
-import { getContacts } from 'redux/selectors';
-import { getStatusFilter } from 'redux/selectors';
 // import { statusFilter } from 'redux/constants';
 // import { selectStatusFilter } from 'redux/selectors';
 
@@ -14,18 +11,11 @@ export const Filter = () => {
   const dispatch = useDispatch();
   // const filter = useSelector(selectStatusFilter);
 
-  const contacts = useSelector(getContacts);
-  const statusFilter = useSelector(getStatusFilter);
-
   const handleFilterChange = filter => dispatch(setStatusFilter(filter));
 
   // const handleFilter = event => {
   //   dispatch(setStatusFilter(event.target.value));
   // };
-
-  const filteredContacts = contacts.filter(contact => {
-    return contact.name.toLowerCase().includes(statusFilter.toLowerCase());
-  });
 
   return (
     <>
